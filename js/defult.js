de56b75e1d269d0e261b9e3082fe065913aca18d
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-01-15 15:28:06 +0900

    Add files via upload

--- js/defult.js
+++ js/defult.js
@@ -1,5 +1,23 @@
-function disBlock(event,_target){
-    event.addEventListener('click',function(){
-    _target.style.display = 'block';
+function modalBlock(eventEle,_target){
+    eventEle.addEventListener('click',function(){
+        _target.style.display = 'block';
     })
 }
+
+// function modalBlockOp(event,self){
+//     for(let idx=0; idx<event.length; idx++){
+//         event[idx].addEventListener('click'),function(){
+//             self.style.display = 'block';
+//         }
+//     }
+    
+// }
+
+function modalNone(eventEleTarget){
+    for(let idx=0;idx<eventEleTarget.length;idx++){
+        eventEleTarget[idx].addEventListener('click',function(){
+            _modal01.style.display = 'none'; 
+        })
+    }    
+}
+
