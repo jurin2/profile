a007cf72a27f7a98cada87a8a04bcd2f6933ad0a
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-01-15 17:12:27 +0900

    Add files via upload

--- js/defult.js
+++ js/defult.js
@@ -1,23 +1,33 @@
-function modalBlock(event,_target){
-    event.addEventListener('click',function(){
-        _target.style.display = 'block';
-    })
-}
-
-// function modalBlockOp(event,self){
-//     for(let idx=0; idx<event.length; idx++){
-//         event[idx].addEventListener('click'),function(){
-//             self.style.display = 'block';
-//         }
-//     }
-    
-// }
-
-function modalNone(eventTarget){
-    for(let idx=0;idx<eventTarget.length;idx++){
-        eventTarget[idx].addEventListener('click',function(){
-            _modal01.style.display = 'none'; 
-        })
-    }    
-}
-
+ // photo
+ let pic = document.getElementsByClassName("pic");
+ // Modal
+ let modals = document.getElementsByClassName("modal");
+ // Modal close
+ let close = document.getElementsByClassName("close");
+ let funcs = [];
+ 
+ // Modal 함수
+ function Modal(num) {
+     return function() {
+         // 해당 클래스의 내용을 클릭하면 Modal을 띄운다.
+         pic[num].addEventListener('click',function(){
+             modals[num].style.display = "block";
+             console.log(num);   
+         });
+     
+         // close를 클릭하면 Modal이 닫힌다.
+         close[num].addEventListener('click',function(){
+             modals[num].style.display = "none";
+         }) 
+     };
+ }
+ 
+ // 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의
+ for(let idx = 0; idx < pic.length; idx++) {
+     funcs[idx] = Modal(idx);
+ }
+ 
+ // 원하는 Modal 수만큼 funcs 함수를 호출합니다.
+ for(let idx2 = 0; idx2 < pic.length; idx2++) {
+     funcs[idx2]();
+ }
